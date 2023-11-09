e8d3191d3ea8a21eced725e6fe9734945cb5e4fe
FrisonJr <jair.frison@gmail.com>
2023-11-09 13:40:46 -0300

    products

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+import { CellAction } from "./cell-action";
+import { CheckCircle, XCircle } from "lucide-react";
+
+export type ProductColumn = {
+  id: string;
+  name: string;
+  price: string;
+  category: string;
+  size: string;
+  color: string;
+  createdAt: string;
+  isFeatured: boolean;
+  isArchived: boolean;
+};
+
+export const columns: ColumnDef<ProductColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    accessorKey: "isArchived",
+    header: "Arquivado",
+    cell: ({ row }) => (
+      <div className="pl-5">
+        {row.original.isArchived ? (
+          <CheckCircle color="#8ED63C" />
+        ) : (
+          <XCircle color="#de0a26" />
+        )}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "isFeatured",
+    header: "Destaque",
+    cell: ({ row }) => (
+      <div className="pl-5">
+        {row.original.isFeatured ? (
+          <CheckCircle color="#8ED63C" />
+        ) : (
+          <XCircle color="#de0a26" />
+        )}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "price",
+    header: "Preço",
+  },
+  {
+    accessorKey: "category",
+    header: "Categoria",
+  },
+  {
+    accessorKey: "size",
+    header: "Tamanho",
+  },
+  {
+    accessorKey: "color",
+    header: "Cor",
+    cell: ({ row }) => (
+      <div className="flex items-center gap-x-2">
+        {row.original.color}
+        <div
+          className="h-6 w-6 rounded-full border"
+          style={{ backgroundColor: row.original.color }}
+        />
+      </div>
+    ),
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Data",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />,
+  },
+];
