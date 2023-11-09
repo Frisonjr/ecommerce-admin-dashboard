b66e03cea79ea4dfabfe95fa460664d718da393f
FrisonJr <jair.frison@gmail.com>
2023-11-09 13:41:01 -0300

    apis

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -1,18 +1,20 @@
-import { auth } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
+import { auth } from '@clerk/nextjs';
 
-import prismadb from "@/lib/prismadb";
+import prismadb from '@/lib/prismadb';
 
-export async function POST(req: Request) {
+export async function POST(
+  req: Request,
+) {
   try {
     const { userId } = auth();
     const body = await req.json();
 
-    if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
-
     const { name } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
 
     if (!name) {
       return new NextResponse("Name is required", { status: 400 });
@@ -22,13 +24,12 @@ export async function POST(req: Request) {
       data: {
         name,
         userId,
-      },
+      }
     });
-
+  
     return NextResponse.json(store);
-
   } catch (error) {
-    console.log(`[STORES_POST]`, error);
-    return new NextResponse("Internal Error", { status: 500 });
+    console.log('[STORES_POST]', error);
+    return new NextResponse("Internal error", { status: 500 });
   }
-}
+};
